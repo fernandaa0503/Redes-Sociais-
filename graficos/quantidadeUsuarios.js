8c86694b70167d4a8268cffef684024a968a95f1
fernandaa0503 <105054880+fernandaa0503@users.noreply.github.com>
2024-09-27 16:40:27 +0000

    .

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-async function quantidadeUsuarios() {
+async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
